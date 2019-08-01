0480c36ba594b15e856a20e452f7c0f6257cbd21
mygetzu <mbenypangestu@gmail.com>
2019-08-01 22:07:49 +0700

    Update - model

--- src/app/rules/rules.controller.ts
+++ src/app/rules/rules.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Req } from '@nestjs/common';
+import { RulesService } from './rules.service';
+import { ApiResponse, ApiOperation } from '@nestjs/swagger';
+import { GetRulesDto } from './dto/get-rules.dto';
 
-@Controller('rules')
-export class RulesController {}
+@Controller('api/v1/rules')
+export class RulesController {
+    constructor(private readonly rulesService: RulesService) {}
+
+    @Get()
+    @ApiResponse({ status: HttpStatus.OK, type: GetRulesDto, description: 'Success!' })
+    @ApiResponse({ status: HttpStatus.NOT_FOUND, description: 'rule not found.' })
+    @ApiResponse({ status: HttpStatus.UNAUTHORIZED, description: 'Unauthorized.' })
+    @ApiOperation({ title: 'Get rule', description: 'Get get rule from JWT payload.' })
+    async findAll(@Req() req): Promise<GetRulesDto[]> {
+        const ruleList = (await this.rulesService.findAll()).map(rule => new GetRulesDto(rule));
+        return Promise.resolve(ruleList);
+    }
+}
