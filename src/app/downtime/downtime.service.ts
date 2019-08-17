357abf31d5a3a65fe8d2ad51fafdf0f01ebda1a4
mygetzu <mbenypangestu@gmail.com>
2019-08-17 11:39:24 +0700

    Update - Line, Downtime

--- src/app/downtime/downtime.service.ts
+++ src/app/downtime/downtime.service.ts
@@ -70,6 +70,31 @@ export class DowntimeService {
         return downtime;
     }
 
+    public async findByCategory(category_id : number, params : DowntimeGetbylineCmd): Promise<any> {
+        let downtime: Downtime[];
+      
+        try {
+            downtime = await this.downtimeRepository
+                                .createQueryBuilder("downtime")
+                                .select(['downtime', 'machine', 'downtime_category', 'downtime_reason', 'line', 'shift'])
+                                .innerJoin("downtime.machine", "machine")
+                                .innerJoin("downtime.downtime_category", "downtime_category")
+                                .innerJoin("downtime.downtime_reason", "downtime_reason")
+                                .innerJoin("downtime.line", "line")
+                                .innerJoin("downtime.shift", "shift")
+                                .andWhere("line.id = :value1", {value1 : params.line_id})
+                                .andWhere("downtime_category.id = :value2", {value2 : category_id})
+                                .orderBy("downtime.created_at", "DESC")
+                                .limit(20)
+                                .getMany();
+        } catch (error) {}
+
+        if (!downtime) {
+            return Utils.NULL_RETURN;
+        }
+        return downtime;
+    }
+
     public async findDowntimeEvent(line_id : number, params : OeeShiftDateShiftCmd): Promise<any> {
         let data: any;
         let rawQuery = "SELECT b.reason as reason," +
